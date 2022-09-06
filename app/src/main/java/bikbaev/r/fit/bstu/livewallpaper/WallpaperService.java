447072d66fbdc112f59aa2a0165704ce936b8187
Rafael <rafael.bikbayev@gmail.com>
2022-09-06 23:45:30 +0300

    Initial commit

---
+++ app/src/main/java/bikbaev/r/fit/bstu/livewallpaper/WallpaperService.java
@@ -0,0 +1,56 @@
+package bikbaev.r.fit.bstu.livewallpaper;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.SurfaceHolder;
+
+public class WallpaperService extends android.service.wallpaper.WallpaperService {
+    @Override
+    public Engine onCreateEngine() {
+        return new WallpaperEngine();
+    }
+
+    class WallpaperEngine extends Engine {
+        public WallpaperEngine() {
+            super();
+        }
+
+        @Override
+        public void onCreate(SurfaceHolder surfaceHolder) {
+            super.onCreate(surfaceHolder);
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+        }
+
+        @Override
+        public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            super.onSurfaceChanged(holder, format, width, height);
+        }
+
+        @Override
+        public void onSurfaceRedrawNeeded(SurfaceHolder holder) {
+            super.onSurfaceRedrawNeeded(holder);
+            Canvas canvas = holder.lockCanvas();
+            canvas.drawColor(Color.RED);
+            holder.unlockCanvasAndPost(canvas);
+        }
+
+        @Override
+        public void onSurfaceCreated(SurfaceHolder holder) {
+            super.onSurfaceCreated(holder);
+        }
+
+        @Override
+        public void onSurfaceDestroyed(SurfaceHolder holder) {
+            super.onSurfaceDestroyed(holder);
+        }
+    }
+}
